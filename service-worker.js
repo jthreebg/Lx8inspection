b9852625974872a34b239cfb4965290634dbc742
jthreebg <jthreebg@gmail.com>
2026-08-25 15:54:42 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,18 @@
 
-const CACHE_NAME = "lx8-inspect-v1";
-const APP_SHELL = [
+const CACHE = "lx8-inspect-v2";
+const SHELL = [
   "./",
   "./index.html",
   "./manifest.webmanifest",
-  "./icon.svg"
+  "./icon-192.png",
+  "./icon-512.png",
+  "./apple-touch-icon.png"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(APP_SHELL))
+    caches.open(CACHE)
+      .then(cache => cache.addAll(SHELL))
       .then(() => self.skipWaiting())
   );
 });
@@ -18,10 +20,7 @@ self.addEventListener("install", event => {
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(key => key !== CACHE_NAME)
-            .map(key => caches.delete(key))
-      )
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
     ).then(() => self.clients.claim())
   );
 });
@@ -34,9 +33,10 @@ self.addEventListener("fetch", event => {
       if (cached) return cached;
 
       return fetch(event.request).then(response => {
-        if (response.ok && new URL(event.request.url).origin === self.location.origin) {
+        if (response.ok &&
+            new URL(event.request.url).origin === self.location.origin) {
           const copy = response.clone();
-          caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+          caches.open(CACHE).then(cache => cache.put(event.request, copy));
         }
         return response;
       }).catch(() => caches.match("./index.html"));
