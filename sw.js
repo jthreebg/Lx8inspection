79d7e36f88ebf7754731bbeacaaf5c8c5481bf1e
jthreebg <jthreebg@gmail.com>
2026-09-02 21:55:31 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'lematic-pwa-v21';
+const CACHE = 'lematic-pwa-v22';
 self.addEventListener('install', (e) => { self.skipWaiting(); });
 self.addEventListener('activate', (e) => {
   e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
